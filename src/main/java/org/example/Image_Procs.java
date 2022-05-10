733ece9a0c21e261d6c5030262e8a49a92fd20df
meman0303 <gabriel.w.weaver@gmail.com>
2022-05-10 15:37:45 -0400

    I fixed up the image_procs class
    I fixed up the image_procs class

---
+++ src/main/java/org/example/Image_Procs.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import net.sourceforge.tess4j.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+public class Image_Procs {
+
+    public static boolean Detect_Valid(Word word, String[] proven, int place, boolean debug) {
+        // reading the image as a whole is far more reliable than reading the character sepretly.
+
+
+    return true;}
+
+
+
+    public static void OCR_Char(String image_path,String data_path,boolean debug) throws Exception {
+        //makes the Tesseract instance and fetches the image.
+        ITesseract instance = new Tesseract();
+        BufferedImage picture = ImageIO.read(new File(image_path));
+        instance.setDatapath(data_path);
+
+
+
+        Graphics2D g = (Graphics2D) picture.getGraphics();
+        g.setStroke(new BasicStroke(3));
+        g.setColor(Color.BLUE);
+        int count=0;
+        for (Word word : instance.getWords(picture, ITessAPI.TessPageIteratorLevel.RIL_SYMBOL)) {
+            //String image_path,Rectangle bound,int scale, int threshold
+            if (/*true*/true) {
+                Rectangle rect = word.getBoundingBox();
+
+
+
+                if (debug){
+                DecimalFormat df = new DecimalFormat("##.");
+                g.drawString(word.getText() + "-" + df.format(word.getConfidence()), (int) rect.getMinX() - 10, (int) rect.getMaxY() + 20);
+                g.draw(rect);}
+
+            }}
+        if (debug){
+        JLabel picLabel = new JLabel(new ImageIcon(picture));
+        JPanel jPanel = new JPanel();
+        jPanel.add(picLabel);
+        JFrame f = new JFrame();
+        f.setSize(new Dimension(picture.getWidth(), picture.getHeight()));
+        f.add(jPanel);
+        f.setVisible(true);}
+
+
+    }
+
+    //This colects the words in the Key.
+    public static String[] OCR_Key(String image_path,String data_path,boolean debug){
+        String[] temp = new String[0];
+
+
+    return(temp);}
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,26 +4,13 @@ package org.example;
 //find letter location
 //https://stackoverflow.com/questions/7314573/how-to-get-coordinates-of-recognized-characters#:~:text=Try%20the%20executable%20of%20tesseract.%20Use%20the%20command,the%20coordinates%20of%20each%20character%2C%20one%20per%20row.
 
-import com.sun.jna.Pointer;
-import net.sourceforge.tess4j.*;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.nio.IntBuffer;
-import java.text.DecimalFormat;
-import java.util.Arrays;
-
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception{
-        String image_path = "wordsearch.jpg";
+        String image_path = "cross_puzzle_test.png";
         String data_path = "tessdata";
-        image_procs.OCR(image_path,data_path,false);
+        Image_Procs.OCR_Char(image_path,data_path,true);
 }
 }
