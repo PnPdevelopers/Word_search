93e2e484bd99ed7e15958c6242f01b053acfb8dd
meman0303 <gabriel.w.weaver@gmail.com>
2022-06-09 09:53:25 -0400

    Removed problems

--- src/main/java/org/example/Image_Procs.java
+++ src/main/java/org/example/Image_Procs.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,41 +18,41 @@ import static net.sourceforge.tess4j.ITessAPI.TessPageSegMode.PSM_SINGLE_CHAR;
 
 public class Image_Procs {
 
-    public static boolean Detect_Valid(Word word, String[] proven, int place, boolean debug) {
+    //public static boolean Detect_Valid(Word word, String[] proven, int place, boolean debug) {
         // reading the image as a whole is far more reliable than reading the character sepretly.
 
 
-    return true;}
+    //return true;}
 
 
 
-    public static void OCR_Char(String image_path,String data_path,boolean debug) throws Exception {
+    //public static void OCR_Char(String image_path,String data_path,boolean debug) throws Exception {
         //makes the Tesseract instance and fetches the image.
-        ITesseract instance = new Tesseract();
-        BufferedImage picture = ImageIO.read(new File(image_path));
-        instance.setDatapath(data_path);
-        instance.setPageSegMode(PSM_SINGLE_CHAR);
-        List<Word> rows = new ArrayList<Word>(); //Initializes the List<Word> rows, which has an object for each line of the puzzle
-        rows = instance.getWords(picture, ITessAPI.TessPageIteratorLevel.RIL_TEXTLINE);
+        //ITesseract instance = new Tesseract();
+        //BufferedImage picture = ImageIO.read(new File(image_path));
+        //instance.setDatapath(data_path);
+        //instance.setPageSegMode(PSM_SINGLE_CHAR);
+        //List<Word> rows = new ArrayList<Word>(); //Initializes the List<Word> rows, which has an object for each line of the puzzle
+        //rows = instance.getWords(picture, ITessAPI.TessPageIteratorLevel.RIL_TEXTLINE);
         
-        float max = 0;
+        //float max = 0;
         
-        for (Word word : rows) {
-            String line = word.getText();
-            line = line.replace('1','I'); //replaces the misread 1's to I's
-            line = line.replaceAll("\\s", ""); //removes phantom spaces
+        //for (Word word : rows) {
+            //String line = word.getText();
+            //line = line.replace('1','I'); //replaces the misread 1's to I's
+            //line = line.replaceAll("\\s", ""); //removes phantom spaces
             
-            if(word.getConfidence()>max){
-                max = word.getConfidence();
-            }
+            //if(word.getConfidence()>max){
+                //max = word.getConfidence();
+            //}
 
 
 
 
 
 
-            System.out.println(line);
-        }
+            //System.out.println(line);
+        //}
         
 
 
@@ -86,7 +87,7 @@ public class Image_Procs {
 
 
 
-    }
+    //}
 /*
     //This colects the words in the Key.
     public static String[] OCR_Key(String image_path,String data_path,boolean debug){
@@ -94,28 +95,33 @@ public class Image_Procs {
 
 
     return(temp);}*/
-    public static int mode(int []array) {
-    HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
-    int max  = 1;
-    int temp = 0;
 
-    for(int i = 0; i < array.length; i++) {
-
-        if (hm.get(array[i]) != null) {
-
-            int count = hm.get(array[i]);
-            count++;
-            hm.put(array[i], count);
-
-            if(count > max) {
-                max  = count;
-                temp = array[i];
-            }
-        }
-
-        else 
-            hm.put(array[i],1);
+    public static char[][] getPuzzle(){
+        char[][] words = {
+                {'W','V','E','R','T','I','C','A','L'},
+                {'R','O','O','A','F','F','L','S','A','B'},
+                {'A','C','R','I','L','I','A','T','O','A'},
+                {'N','D','O','D','K','O','N','W','D','C'},
+                {'D','R','K','E','S','O','O','D','D','K'},
+                {'O','E','E','P','Z','E','G','L','I','W'},
+                {'M','S','I','I','H','O','A','E','R','A'},
+                {'A','L','R','K','R','R','I','R','E','R'},
+                {'K','O','D','I','D','E','D','R','C','D'},
+                {'H','E','L','W','S','L','E','U','T','H'}};
+        return words;
     }
-    return temp;
-}
+    public static List<String> getWords() {
+        List<String> puzzle = Arrays.asList(
+                "SEEK",
+                "FIND",
+                "RANDOM",
+                "SLEUTH",
+                "BACKWARD",
+                "VERTICAL",
+                "DIAGONAL",
+                "WIKIPEDIA",
+                "HORIZONTAL",
+                "WORDSEARCH");
+        return puzzle;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,25 @@ package org.example;
 //find letter location
 //https://stackoverflow.com/questions/7314573/how-to-get-coordinates-of-recognized-characters#:~:text=Try%20the%20executable%20of%20tesseract.%20Use%20the%20command,the%20coordinates%20of%20each%20character%2C%20one%20per%20row.
 
+//find letter location
+//https://stackoverflow.com/questions/7314573/how-to-get-coordinates-of-recognized-characters#:~:text=Try%20the%20executable%20of%20tesseract.%20Use%20the%20command,the%20coordinates%20of%20each%20character%2C%20one%20per%20row.
+import java.util.List;
+import java.util.Arrays;
+
 public class Main {
     //Had to change Main to not be static to account for the GUI. Just doing what the IDE told me as a suggestion.
     public static void main(String[] args) throws Exception {
-        String image_path = "cross_puzzle_test.png";
-        String data_path = "tessdata";
-        Image_Procs.OCR_Char(image_path, data_path, true);
+        //String image_path = "cross_puzzle_test.png";
+        //String data_path = "tessdata";
+        //Image_Procs.OCR_Char(image_path, data_path, true);
+        char[][] puzzle = Image_Procs.getPuzzle();
+        List<String> words = Image_Procs.getWords();
+        for(int i = 0; i<= words.size()-1; i++){
+            GFG.patternSearch(puzzle,words.get(i));
+        }
+
         GUI.main(null);
     }
 }
+
 
