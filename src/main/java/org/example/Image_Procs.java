38ad455d1aff3711ce1c43ba5ce9288cdf53f01a
meman0303 <gabriel.w.weaver@gmail.com>
2022-06-09 15:22:53 -0400

    I made comments in image Procs.

--- src/main/java/org/example/Image_Procs.java
+++ src/main/java/org/example/Image_Procs.java
@@ -18,84 +18,56 @@ import static net.sourceforge.tess4j.ITessAPI.TessPageSegMode.PSM_SINGLE_CHAR;
 
 public class Image_Procs {
 
-    //public static boolean Detect_Valid(Word word, String[] proven, int place, boolean debug) {
-        // reading the image as a whole is far more reliable than reading the character sepretly.
-
-
-    //return true;}
 
 
 
-    //public static void OCR_Char(String image_path,String data_path,boolean debug) throws Exception {
+    public static void OCR_Char(String image_path,String data_path,boolean debug) throws Exception {
         //makes the Tesseract instance and fetches the image.
-        //ITesseract instance = new Tesseract();
-        //BufferedImage picture = ImageIO.read(new File(image_path));
-        //instance.setDatapath(data_path);
+        ITesseract instance = new Tesseract();
+        //primes the image for use.
+        BufferedImage picture = ImageIO.read(new File(image_path));
+        //sets the data path for the tried data
+        instance.setDatapath(data_path);
+
+        //This would be used if we are getting just one character.
         //instance.setPageSegMode(PSM_SINGLE_CHAR);
-        //List<Word> rows = new ArrayList<Word>(); //Initializes the List<Word> rows, which has an object for each line of the puzzle
-        //rows = instance.getWords(picture, ITessAPI.TessPageIteratorLevel.RIL_TEXTLINE);
-        
-        //float max = 0;
-        
-        //for (Word word : rows) {
-            //String line = word.getText();
-            //line = line.replace('1','I'); //replaces the misread 1's to I's
-            //line = line.replaceAll("\\s", ""); //removes phantom spaces
-            
-            //if(word.getConfidence()>max){
-                //max = word.getConfidence();
-            //}
 
+        //Makes the list for the output
+        List<Word> rows = new ArrayList<Word>(); //Initializes the List<Word> rows, which has an object for each line of the puzzle
+        //list of all the letters line by line in HOCR format
+        rows = instance.getWords(picture, ITessAPI.TessPageIteratorLevel.RIL_TEXTLINE);
 
+        //sets max confidence level container.
+        float max = 0;
+        // Error detection removing all spaces and changes all "1" to "I"
+        for (Word word : rows) {
+            String line = word.getText();
+            line = line.replace('1','I'); //replaces the misread 1's to I's
+            line = line.replaceAll("\\s", ""); //removes phantom spaces
 
-
-
-
-            //System.out.println(line);
-        //}
-        
-
-
-        /*Graphics2D g = (Graphics2D) picture.getGraphics();
+            // Used to find the max confidence.
+            if(word.getConfidence()>max){
+                max = word.getConfidence();
+            }
+            System.out.println(line);}
+        //used to debug the process for error detection.
+            if (debug){
+                Graphics2D g = (Graphics2D) picture.getGraphics();
         g.setStroke(new BasicStroke(3));
         g.setColor(Color.BLUE);
-        int count=0;*/
-        /*
-        for (Word word : instance.getWords(picture, ITessAPI.TessPageIteratorLevel.RIL_SYMBOL)) {
-            //String image_path,Rectangle bound,int scale, int threshold
-            if (true) {
-                Rectangle rect = word.getBoundingBox();
 
 
-
-                if (debug){
-                DecimalFormat df = new DecimalFormat("##.");
-                g.drawString(word.getText() + "-" + df.format(word.getConfidence()), (int) rect.getMinX() - 10, (int) rect.getMaxY() + 20);
-                g.draw(rect);}
-
-            }}*/
-
-
-       /* if (debug){
         JLabel picLabel = new JLabel(new ImageIcon(picture));
         JPanel jPanel = new JPanel();
         jPanel.add(picLabel);
         JFrame f = new JFrame();
         f.setSize(new Dimension(picture.getWidth(), picture.getHeight()));
         f.add(jPanel);
-        f.setVisible(true);}*/
+        f.setVisible(true);}
+            //end of debug code
+            }
 
-
-
-    //}
-/*
-    //This colects the words in the Key.
-    public static String[] OCR_Key(String image_path,String data_path,boolean debug){
-        String[] temp = new String[0];
-
-
-    return(temp);}*/
-
+    //Backup shortcut list of letter as a word search to be used if the ocr is not working
     public static char[][] getPuzzle(){
         char[][] words = {
                 {'W','V','E','R','T','I','C','A','L'},
@@ -110,6 +82,8 @@ public class Image_Procs {
                 {'H','E','L','W','S','L','E','U','T','H'}};
         return words;
     }
+
+    //Backup shortcut list of key words to be used if the ocr is not working
     public static List<String> getWords() {
         List<String> puzzle = Arrays.asList(
                 "SEEK",
